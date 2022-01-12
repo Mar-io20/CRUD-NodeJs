61a730adf82dbeebef48b5b689450cab8e43154c
Mario Gabriel PC <mario.contate.me@gmail.com>
2022-01-12 14:14:13 -0300

    Criando style

--- CRUD-3.0/client/src/App.js
+++ CRUD-3.0/client/src/App.js
@@ -1,9 +1,30 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      Ola mundo
+    <div className="app--container">
+      <div className="register--container">
+        <h1>Scrim Shop</h1>
+        <input
+          type="text"
+          name="name"
+          placeholder="Nome"
+          className="register--input"
+        />
+        <input
+          type="text"
+          name="cost"
+          placeholder="Preço"
+          className="register--input"
+        />
+        <input
+          type="text"
+          name="category"
+          placeholder="Categoria"
+          className="register--input"
+        />
+        <button>Cadastrar</button>
+      </div>
     </div>
   );
 }
